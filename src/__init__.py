f6a9a7cb73508d2595b4789ac5e906a4bcd669db
LinaMezghani <lina.mezghani@gmail.com>
2024-01-09 17:22:51 +0100

    feat: add front

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from .main import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
